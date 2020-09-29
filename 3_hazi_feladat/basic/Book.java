b77a4a891e3e3699db933e154a9203650d28ffdd
gulyassimone <gulyassimone@gmail.com>
2020-09-29 17:45:10 +0000

    3_hazi_feladat

---
+++ 3_hazi_feladat/basic/Book.java
@@ -0,0 +1,24 @@
+package basic;
+
+public class Book{
+
+  String title;
+  String author;
+  int year;
+  boolean available;
+
+  public Book(String title,String author,int year,boolean available){
+    if(!(year > 0 && year < 2020)){
+      throw new IllegalArgumentException("Invalid year");
+    }
+    this.title = title;
+    this.author = author;
+    this.year= year;
+    this.available = available;
+  }
+
+  @Override
+  public String toString(){
+    return ((available?"Available ":"Not available " ) + title + " written by " + author + " in " + year);
+  }
+}
